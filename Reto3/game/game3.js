bd051fcfc59450866a96afec25b0c307e9a9eede
Jorge <jhermo@SECUOYA.COM>
2023-01-12 11:44:11 +0100

    delet deprecated code

--- Reto3/game/game3.js
+++
@@ -1,8 +0,0 @@
-startButton.addEventListener("click", function () {
-    for (let i = 0; i < 5; i++) {
-        obstacle.setAttribute("color", " red")
-        obstacle.setAttribute("scale", "0.5 0.5 0.5")
-        obstacle.setAttribute("position", `${getRandomPosition()}`)
-        obstacle.setAttribute("dynamic.body", "")
-    }
-})
